4151716773b75ed7e32c96acc64a7c1f55173a36
Abdurakhman R. <joha.shadibekov@gmail.com>
2025-09-02 00:15:01 +0500

    [FIX] - fix unused types for handlers;
    Signed-off-by: Abdurakhman R. <joha.shadibekov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'dart:async';
-import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter/services.dart';
 import 'package:jabook/app/app.dart';
 import 'package:jabook/core/logging/environment_logger.dart';
 
 void main() {
   // Setup global error handling
-  FlutterError.onError = (FlutterErrorDetails details) {
+  FlutterError.onError = (details) {
     logger.e('Flutter error: ${details.exception}', stackTrace: details.stack);
   };
 
