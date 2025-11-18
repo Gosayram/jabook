795b8ed2bb023f8194bcf8363a0864293483a121
Abdurakhman R. <joha.shadibekov@gmail.com>
2025-11-18 10:22:31 +0500

    [ADD] - async health check and logging optimization; added License for each code files; added metrics for network connections, validaitons and fix crashing after starting application;
    Signed-off-by: Abdurakhman R. <joha.shadibekov@gmail.com>

--- lib/core/metadata/audiobook_metadata_service.dart
+++ lib/core/metadata/audiobook_metadata_service.dart
@@ -1,3 +1,17 @@
+// Copyright 2025 Jabook Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:jabook/core/logging/structured_logger.dart';
 import 'package:jabook/core/net/dio_client.dart';
 import 'package:jabook/core/parse/rutracker_parser.dart';
