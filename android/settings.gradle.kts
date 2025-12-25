04f03ca6607b7c2b40dbe7eb729f86286bcfb699
Abdurakhman R. <abdurakhman.rahmankulov@gmail.com>
2025-12-25 19:26:54 +0500

    [FIX] - Remove unnecessary Flutter plugin loader from settings.gradle.kts; streamline project configuration;

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,20 +1,8 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
-        }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-
     }
 }
 
@@ -28,7 +16,8 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    // REMOVED: Flutter plugin loader - no longer needed
+    // id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.13.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.3.3" apply false
