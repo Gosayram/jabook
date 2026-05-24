3e281cbc1134e26a60fe17ec5098c4d0fca5a4fa
Abdurakhman Rakhmankulov <joha.shadibekov@gmail.com>
2026-05-25 02:56:34 +0500

    [UPD] - update android instrumentation test deps and versions;

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -606,6 +606,13 @@ dependencies {
     testImplementation(libs.jimfs)
     testImplementation(libs.kotest.property)
 
+    // Android Instrumentation tests
+    androidTestUtil(libs.androidx.test.services)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.media3.test.utils)
+
     // Firebase - Import the Firebase BoM to manage library versions
     implementation(platform(libs.firebase.bom))
 
